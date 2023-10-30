53bba4884cfcbaeb2206f7e80f0aba8380a51f5f
Kiara Saahfon <ksaah025@uottawa.ca>
2023-10-30 12:09:22 -0400

    more
    committing more

---
+++ app/src/main/java/com/example/doctorregistration/pendingList.java
@@ -0,0 +1,64 @@
+package com.example.doctorregistration;
+import java.util.ArrayList;
+
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.os.Bundle;
+
+import com.google.firebase.Firebase;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+
+public class pendingList extends AppCompatActivity {
+
+    RecyclerView recyclerView;
+    DatabaseReference database;
+    MyAdapter myAdapter;
+    ArrayList<Doctor> list;
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_pending_list);
+
+        recyclerView = findViewById(R.id.pendingList);
+        database = FirebaseDatabase.getInstance().getReference("Pending Requests");
+        recyclerView.setHasFixedSize(true);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        list = new ArrayList<>();
+        myAdapter = new MyAdapter(this,list);
+        recyclerView.setAdapter(myAdapter);
+
+        database.addValueEventListener(new ValueEventListener(){
+
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+
+                for(DataSnapshot dataSnapshot: snapshot.getChildren()){
+                    Doctor user = dataSnapshot.getValue(Doctor.class);
+                    list.add(user);
+                }
+
+                myAdapter.notifyDataSetChanged();
+
+            }
+
+
+
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
+
+
+    }
+}
