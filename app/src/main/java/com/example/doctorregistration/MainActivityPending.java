96786478af141d846a8b8242d344a2bc36f7b625
Jeny Patel <jenypatel2203@gmail.com>
2023-10-30 22:26:24 -0400

    Database Pending List

--- app/src/main/java/com/example/doctorregistration/MainActivityPending.java
+++ app/src/main/java/com/example/doctorregistration/MainActivityPending.java
@@ -1,8 +1,13 @@
 package com.example.doctorregistration;
 
+import android.annotation.SuppressLint;
 import android.app.ProgressDialog;
+import android.content.Context;
 
 import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import com.google.firebase.firestore.DocumentChange;
 import com.google.firebase.firestore.EventListener;
@@ -10,7 +15,9 @@ import com.google.firebase.firestore.FirebaseFirestore;
 import com.google.firebase.firestore.FirebaseFirestoreException;
 import com.google.firebase.firestore.QuerySnapshot;
 
-public class MainActivityPending {
+import java.util.ArrayList;
+
+public class MainActivityPending extends AppCompatActivity {
     RecyclerView recyclerView;
     ArrayList<Patient> patientArrayList;
 
@@ -18,32 +25,37 @@ public class MainActivityPending {
 
     FirebaseFirestore db;
 
-    protected void onCreate( Bundle savedInstanceState){
+    @SuppressLint("MissingInflatedId")
+    protected void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_mainactivitypending);
 
-        ProgressDialog progessDialog = new ProgressDialog(this);
-        progressDialogsetCancelable(false);
-        progressDialog.setMessage("Fetching data");
+        ProgressDialog progressDialog = new ProgressDialog(this);
+        progressDialog.setCancelable(false);
+        progressDialog.setMessage("Fetching Data");
         progressDialog.show();
 
-        recyclerView = findViewByID(recyclerview);
+
+        recyclerView = findViewById(R.id.recyclerView);
         recyclerView.setHasFixedSize(true);
-        recyclerView.setLayoutManager(new LinearLayoutManager (this));
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
 
         db = FirebaseFirestore.getInstance();
-        patientArrayList = new ArrayList<Patient>();
-        myAdapter = new MyAdapterPatient(MainActivityPending.this, userArrayList);
+        patientArrayList = new ArrayList<>();
+        myAdapter = new MyAdapterPatient(MainActivityPending.this, patientArrayList);
 
         recyclerView.setAdapter(myAdapter);
 
+
+        // Populate the pendingRequestsList with data from Firebase Firestore
         EventChangeListener();
 
 
 
     }
     private void EventChangeListener(){
-        db.collection("user")
+
+        db.collection("pending reqeuest")
                 .addSnapshotListener(new EventListener<QuerySnapshot>() {
                     @Override
                     public void onEvent(@Nullable QuerySnapshot value, @Nullable FirebaseFirestoreException error) {
@@ -55,7 +67,7 @@ public class MainActivityPending {
                             Log.e("Firebase error", error.getMessage());
                             return;
                         }
-                        for (DocumentChange dc : value.getDocumentChanges()) {
+                        for(DocumentChange dc : value.getDocumentChanges()) {
                             if (dc.getType() == DocumentChange.Type.Added) {
                                 patientArrayList.add(dc.getDocument().toObject(Patient.class));
                             }
