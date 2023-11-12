003249587872f069333e9b24b0d2422dc0668fe0
ess-dee <sdebn078@uottawa.ca>
2023-11-12 18:36:49 -0500

    month calendar

---
+++ app/src/main/java/com/example/doctorregistration/WeekViewActivity.java
@@ -0,0 +1,21 @@
+package com.example.doctorregistration;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+
+public class WeekViewActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_week_view);
+    }
+
+    public void previousWeekAction(View view) {
+    }
+
+    public void nextWeekAction(View view) {
+    }
+}
