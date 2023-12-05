c7e1d03de1ab74f474b4b3d354a56cf88e0cd5e0
UpamaU <uutpa088@uottawa.ca>
2023-12-04 22:46:28 -0500

    deleting old files

--- app/src/main/java/com/example/doctorregistration/Patient/Frontend/PatientWelcome.java
+++ app/src/main/java/com/example/doctorregistration/Patient/Frontend/PatientWelcome.java
@@ -8,7 +8,6 @@ import android.view.View;
 import android.widget.Button;
 
 import com.example.doctorregistration.Launcher.MainWelcome;
-import com.example.doctorregistration.Patient.Backend.PatientAppointmentsList;
 import com.example.doctorregistration.R;
 import com.google.firebase.auth.FirebaseAuth;
 
